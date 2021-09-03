2400a8626e1dd04229022a380ff1085687137eca
Ivan Chernykh <ivanchernykh98@gmail.com>
2021-09-03 20:42:01 +0300

    first commit, created files and folders

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import cls from './NavBar.module.scss'
+
+const NavBar = () => {
+    return (
+        <div className={cls.navbar}>
+            This is navbar
+        </div>
+    )
+}
+
+export default NavBar
